f6dda231160108ba6e7c3901b19a2164cd92895d
Ronny Gaertner <ronny.gaertner@googlemail.com>
2021-11-26 14:37:06 +0100

    feat: add key tests

--- src/key.py
+++ src/key.py
@@ -1,37 +1,87 @@
+from dataclasses import dataclass
+import uuid
 import os
-import typing
+import re
+import base58
+from typing import Optional
 
 from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
+from cryptography.hazmat.primitives import hashes
 
+@dataclass
+class SymetricKeyProps:
+    nonce: Optional[bytes] = None
+    key: Optional[bytes] = None
+    hashSeed: str = str(uuid.uuid4())
+    key_length: int = 32
+    nonce_length: int = 12
 
 class SymetricKey:
+    _id: Optional[str]
+    _hashSeed: str
+    _key: Optional[bytes]
+    _nonce: Optional[bytes]
+    _alg = 'chacha20-poly1305'
+    _key_length: int
+    _nonce_length: int
 
-    def __init__(self,nounce: typing.Optional[bytes], key:
-                 typing.Optional[bytes]):
-        self._key = key
-        self._nonce = nounce
+    def __init__(self, props: SymetricKeyProps):
+        self._key = props.key
+        self._nonce = props.nonce
+        self._hashSeed = props.hashSeed
+        self._key_length = props.key_length
+        self._nonce_length = props.nonce_length
 
-    def create(self) -> None:
-        self._key = ChaCha20Poly1305.generate_key()
-        self._nonce = os.urandom(12)
+    @staticmethod
+    def create(props: SymetricKeyProps):
+        return SymetricKey(props).generate()
+
+    def generate(self):
+        self._key = os.urandom(self._key_length)
+        self._nonce = os.urandom(self._nonce_length)
+        digest = hashes.Hash(hashes.SHA256())
+        digest.update(self._key)
+        digest.update(self._nonce)
+        self._id=base58.b58encode(digest.finalize()).decode()
         self._chacha = ChaCha20Poly1305(self._key)
+        return self
 
-    def get_key(self) -> bytes:
+    def create_nonce(self) -> bytes:
+        self._nonce = os.urandom(12)
+        return self.nonce
+
+    @property
+    def key(self) -> bytes:
         if self._key is None:
             raise Exception("key is not set")
         return self._key
 
-    def get_nonce(self) -> bytes:
+    @property
+    def id(self) -> str:
+        if self._id is None:
+            raise Exception("key is not set")
+        return self._id
+
+    @property
+    def nonce(self) -> bytes:
         if self._nonce is None:
             raise Exception("nonce is not set")
         return self._nonce
 
-    def encrypt(self, message: bytes, addition: typing.Optional[bytes] ) -> bytes:
-        return self._chacha.encrypt(self.get_nonce(), message, addition)
+    def encrypt(self, message: bytes, addition: Optional[bytes] ) -> bytes:
+        return self._chacha.encrypt(self.nonce, message, addition)
 
-    def decrypt(self, encrypted: bytes, addition: typing.Optional[bytes] ) -> bytes:
-        return self._chacha.decrypt(self.get_nonce(), encrypted, addition)
+    def decrypt(self, encrypted: bytes, addition: Optional[bytes] ) -> bytes:
+        return self._chacha.decrypt(self.nonce, encrypted, addition)
 
+    def hash(self, enc: str)-> str:
+        hasher = hashes.Hash(hashes.SHA256())
+        hasher.update(bytes(self._hashSeed, 'utf-8'));
+        stripped = re.sub(r'[\n\r\s\t]+','', enc.upper(),flags=(re.M))
+        hasher.update(stripped.encode())
+        # 'A' means sha256 upcase und replace
+        string_hash = base58.b58encode(hasher.finalize()).decode()
+        return  "A{}".format(string_hash)
 
 
 
--- src/key_test.py
+++ src/key_test.py
@@ -1,14 +1,26 @@
 import unittest
 
-from key import SymetricKey
+from key import SymetricKey, SymetricKeyProps
+
 
 class SymetricKeyTest(unittest.TestCase):
     def test_en_decrypt(self) -> None:
         message ="this is the only one message"
-        key = SymetricKey(None, None)
-        key.create()
+        key = SymetricKey.create(SymetricKeyProps())
         encrypted = key.encrypt(message.encode(), None)
         decrypted = key.decrypt(encrypted, None)
         print(decrypted)
         self.assertEqual(message,decrypted.decode())
 
+
+    def test_hash(self) -> None:
+        key_props = SymetricKeyProps(
+            hashSeed= 'Hash',
+            nonce = bytes('////////////', "utf-8"),
+            key = bytes('AQEBAQEBAQEBAQEB', "utf-8"),
+        )
+            # nonce= Buffer.alloc(12, 0xff).toString('base64'),
+            # key= Buffer.alloc(32, 0x01).toString('base64'),
+        key = SymetricKey.create(key_props)
+        self.assertEqual(key.hash('input'), 'AEJDPjgo535AwWJ2tEPt8KW91DDS9JiktH6hsQ2Xq6kLR')
+        self.assertEqual(key.hash(' in\nPut '),'AEJDPjgo535AwWJ2tEPt8KW91DDS9JiktH6hsQ2Xq6kLR')
